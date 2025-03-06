424c9202bc9aa37fda928dda67570c66e7629be0
parsa <parsaalavi9@gmail.com>
2025-03-06 18:31:56 +0330

    add responsive to login and singin

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom"
+
+import { KeyRoundIcon, Phone, User } from "lucide-react"
+
 import Container from "../../components/container/Container"
 import FloatingInput from "../../components/FloatingInput/FloatingInput"
 import loginImg from '../../assets/images/login.png'
 import Button from "../../components/button/Button"
-import { KeyRoundIcon, Phone, User } from "lucide-react"
+import Header from '../../components/header/Header'
+import './login.css'
+
 function Login() {
+
+  return (
+    <>
+      <Laptop className='laptop'></Laptop>
+      <Mobile className='mobile'></Mobile>
+    </>
+  )
+}
+
+function Laptop({ ...rest }) {
   function onsubmitForm(e) {
     e.preventDefault()
-
   }
   return (
     <>
-      <Container>
-        <div className="flex py-12 h-screen justify-between items-center">
+      <Container {...rest}>
+        <div className="flex py-12 h-screen justify-between items-center login_main">
           <div className="w-1/2 flex flex-col justify-between items-center gap-7" >
             <div className="flex flex-col justify-between items-center min-h-20">
               <h2 className="text-3xl text-primary font-bold h-1/2">
@@ -23,32 +37,74 @@ function Login() {
               </h3>
             </div>
             <form onSubmit={(e) => onsubmitForm(e)} className="w-full flex flex-col gap-5 items-center">
-              
+
               <FloatingInput text='شماره موبایل' title='phone' inputType='tel'>
-              <Phone 
+                <Phone
                   className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
                   size={16}
                 />
               </FloatingInput>
               <FloatingInput text='رمز عبور' title='pass' inputType='password'>
-              <KeyRoundIcon
+                <KeyRoundIcon
                   className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
                   size={16}
                 />
               </FloatingInput>
-             
+
               <Button ColorBackground="bg-primary" color="text-white" className='hover:text-primary hover:bg-white border-2 w-full'>ورود</Button>
               <Link to="/singin">
-              ثبت نام نکرده اید؟همین حالا <span className="text-primary">عضو</span> شوید
-             </Link>
+                ثبت نام نکرده اید؟همین حالا <span className="text-primary">عضو</span> شوید
+              </Link>
             </form>
           </div>
           <div className="w-2/5 flex flex-col items-center h-full" >
             <img src={loginImg} alt="" className="rounded-[3rem] h-full object-cover w-full" />
           </div>
         </div>
+      </Container></>
+  )
+}
+function Mobile({ className, ...rest }) {
+  function onsubmitForm(e) {
+    e.preventDefault()
+  }
+  return (
+    <div className={`flex flex-col ${className} relative`} {...rest}>
+      <Header></Header>
+      <div className="w-full flex flex-col items-center " >
+        <img src={loginImg} alt="" className=" h-72 object-cover w-full" />
+      </div>
+      <Container className='absolute bg-white top-72 rounded-3xl '>
+        <div className="flex h-full justify-center items-start">
+          <div className="flex flex-col justify-between items-center w-full py-6 gap-6 " >
+            <h5 className="text-shade2 font-bold text-xl h-1/2 ">
+              ورود
+            </h5>
+            <form onSubmit={(e) => onsubmitForm(e)} className="w-full flex flex-col gap-5 items-center">
+
+              <FloatingInput text='شماره موبایل' title='phone' inputType='tel'>
+                <Phone
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+              <FloatingInput text='رمز عبور' title='pass' inputType='password'>
+                <KeyRoundIcon
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+
+              <Button ColorBackground="bg-primary" color="text-white" className='hover:text-primary hover:bg-white border-2 w-full'>ورود</Button>
+              <Link to="/singin">
+                ثبت نام نکرده اید؟همین حالا <span className="text-primary">عضو</span> شوید
+              </Link>
+            </form>
+          </div>
+
+        </div>
       </Container>
-    </>
+    </div>
   )
 }
 
--- src/pages/singin/Singin.jsx
+++ src/pages/singin/Singin.jsx
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom"
+
+import { KeyRoundIcon, Phone, User } from "lucide-react"
+
 import Container from "../../components/container/Container"
 import FloatingInput from "../../components/FloatingInput/FloatingInput"
 import loginImg from '../../assets/images/login.png'
 import Button from "../../components/button/Button"
-import { KeyRoundIcon, Phone, User } from "lucide-react"
+import Header from '../../components/header/Header'
+import './Singin.css'
+
 function Singin() {
+
+  return (
+    <>
+      <Laptop className='laptop'></Laptop>
+      <Mobile className='mobile'></Mobile>
+    </>
+  )
+}
+
+function Laptop({ ...rest }) {
   function onsubmitForm(e) {
     e.preventDefault()
-
   }
   return (
     <>
-      <Container>
+      <Container {...rest}>
         <div className="flex  py-12 h-screen justify-between items-center">
           <div className="w-1/2 flex flex-col justify-between items-center gap-7" >
             <div className="flex flex-col justify-between items-center min-h-20">
@@ -63,8 +77,66 @@ function Singin() {
             <img src={loginImg} alt="" className="rounded-[3rem] h-full object-cover w-full" />
           </div>
         </div>
+      </Container></>
+  )
+}
+function Mobile({ className, ...rest }) {
+  function onsubmitForm(e) {
+    e.preventDefault()
+  }
+  return (
+    <div className={`flex flex-col ${className} relative`} {...rest}>
+      <Header></Header>
+      <div className="w-full flex flex-col items-center " >
+        <img src={loginImg} alt="" className=" h-72 object-cover w-full" />
+      </div>
+      <Container className='absolute bg-white top-72 rounded-3xl '>
+        <div className="flex h-full justify-center items-start">
+          <div className="flex flex-col justify-between items-center w-full py-6 gap-6 " >
+            <h5 className="text-shade2 font-bold text-xl h-1/2 ">
+              ثبت نام
+            </h5>
+            <form onSubmit={(e) => onsubmitForm(e)} className="w-full flex flex-col gap-5 items-center">
+              <FloatingInput text='نام' title='name'>
+                <User
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+              <FloatingInput text='نام خانوادگی' title='lastName'>
+              <User
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+              <FloatingInput text='ایجاد رمز عبور' title='pass' inputType='password'>
+              <KeyRoundIcon
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+              <FloatingInput text='تکرار رمز عبور' title='pass2' inputType='password'>
+              <KeyRoundIcon
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+              <FloatingInput text='شماره موبایل' title='phone' inputType='tel'>
+              <Phone 
+                  className="absolute right-3 top-[18px] text-gray-500 peer-focus:text-primary"
+                  size={16}
+                />
+              </FloatingInput>
+              <Button ColorBackground="bg-primary" color="text-white" className='hover:text-primary hover:bg-white border-2 w-full'>ثبت نام</Button>
+              <Link to="/login">
+                حساب کاربری دارید <span className="text-primary text-xl">وارد</span> شوید
+              </Link>
+            </form>
+          </div>
+
+        </div>
       </Container>
-    </>
+    </div>
   )
 }
 
